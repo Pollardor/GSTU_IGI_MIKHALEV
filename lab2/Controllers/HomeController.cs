e65f998efb11eac7375b81e692581e3e665767a2
Pollardor <mrpollardor@gmail.com>
2018-04-11 09:16:12 +0300

    lab2
    lab2

---
+++ lab2/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using lab2.Models;
+
+namespace lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public IActionResult GetEmail(string email)
+        {
+            string result ="Post: "+ email + " succes";
+
+            return Json(result);
+        }
+        [HttpGet]
+
+        public IActionResult getInfo(string email)
+        {
+            string result = "Get: " + email + " succes";
+
+            return Json(result);
+        }
+    }
+}
